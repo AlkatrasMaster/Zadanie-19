a19c3fa7abc286526ca94bc4bdcddf7ad28afead
Renat Mavlayviev <AlkatrasMaster@yandex.ru>
2024-05-13 00:20:29 +0500

    Урок 19.1 Изучен

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("data/info.txt"));
+            lines.forEach(line -> builder.append(line + "\n"));
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println(builder.toString());
     }
 }
